3beff5c72ceb1178c003a6258cbec13b62deac77
Axel Barrera <barr33ra@gmail.com>
2019-09-23 09:25:30 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,42 @@
+const { crearArchivo, listartabla } = require(`./Multiplicar/multiplicacion`)
+const argumentos = require(`yargs`)
+    .command(`listar`, `Imprime en la terminar la tabla de multiplicar`, {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: '1',
+            default: 10
+        }
+    })
+    .command(`crear`, `Exporta en un archivo la tabla de multiplicar`, {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        /*limite:{
+                    alias: '1',
+                    default: 10
+                }*/
+    })
+    .help()
+    .argv;
+let base = " ";
+let comandos = argumentos._[0];
+
+switch (comandos) {
+    case `listar`:
+        listartabla(argumentos.base, argumentos.limite);
+        break;
+    case `crear`:
+        creararchivo()
+        break;
+
+    default:
+        console / log(`No se Reconoce el comando ${comandos}`)
+        break;
+}
+/*crearArchivo(base)
+.then(respuesta => console.log(`archivo creado: ${respuesta}`))
+.catch(error => console.log(error));*/
